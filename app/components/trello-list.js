a826d63bf697a9623eaa8bdc2716745a2ee45600
Mike Burden <maburdenjr@gmail.com>
2016-08-01 01:37:46 -0700

    adding tasks and entry form

--- app/components/trello-list.js
+++ app/components/trello-list.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  showAddItemForm: false,
+  actions: {
+    addItem() {
+      this.toggleProperty('showAddItemForm');
+    },
+    cancelAdd() {
+      this.toggleProperty('showAddItemForm');
+    },
+    saveItem() {
+      this.toggleProperty('showAddItemForm');
+    }
+  }
 });
